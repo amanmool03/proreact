00bf194259280da7fa93ab415d18d4c38283cd45
amanmool03 <moolaman10@gmail.com>
2023-12-23 16:59:15 +0545

    #10 added the scripts for testing.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,26 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import App from "./App";
 
 it("should have hello world", () => {
   render(<App />);
-  const message = screen.queryByText(/Hello world/i);
+  const message = screen.getByRole("heading", {
+    name: /hello world!!!!/i,
+  });
   expect(message).toBeVisible();
+});
+
+it("should increment the count when clicking the button", () => {
+  render(<App />);
+
+  // Find the button based on its text content
+  const buttonEl = screen.getByText(/count is 0/i);
+
+  // Check if the button is found
+  expect(buttonEl).toBeInTheDocument();
+
+  // Click the button
+  fireEvent.click(buttonEl);
+
+  // Check if the count updates
+  expect(screen.getByText(/count is 1/i)).toBeInTheDocument();
 });
